b073cda8def3c40092005bb73fc6904c5341480c
Dmytro Holysh <dima.golysh@gmail.com>
2022-10-14 10:37:09 +0200

    improve adding entry popup

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -49,54 +49,61 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(labelText: 'Title'),
-                controller: _titleController,
-                onSubmitted: (_) => _submitData,
-                // onChanged: (value) => _titleInput = value,
-              ),
-              TextField(
-                keyboardType:
-                    const TextInputType.numberWithOptions(decimal: true),
-                decoration: InputDecoration(labelText: 'Amount'),
-                controller: _amountController,
-                onSubmitted: (_) => _submitData,
-                // onChanged: (value) => _amountInput = value,
-              ),
-              Container(
-                height: 70,
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(_selectedDate == null
-                          ? 'No Date Chosen!'
-                          : DateFormat.yMd().format(_selectedDate!)),
-                    ),
-                    TextButton(
-                      onPressed: _presentDatePicker,
-                      style: TextButton.styleFrom(
-                        foregroundColor: Theme.of(context).primaryColor,
-                      ),
-                      child: const Text(
-                        'Choose date',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    )
-                  ],
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText: 'Title'),
+                  controller: _titleController,
+                  onSubmitted: (_) => _submitData,
+                  // onChanged: (value) => _titleInput = value,
                 ),
-              ),
-              ElevatedButton(
-                onPressed: _submitData,
-                child: Text('Add Transaction'),
-              )
-            ]),
+                TextField(
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  decoration: InputDecoration(labelText: 'Amount'),
+                  controller: _amountController,
+                  onSubmitted: (_) => _submitData,
+                  // onChanged: (value) => _amountInput = value,
+                ),
+                Container(
+                  height: 70,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(_selectedDate == null
+                            ? 'No Date Chosen!'
+                            : DateFormat.yMd().format(_selectedDate!)),
+                      ),
+                      TextButton(
+                        onPressed: _presentDatePicker,
+                        style: TextButton.styleFrom(
+                          foregroundColor: Theme.of(context).primaryColor,
+                        ),
+                        child: const Text(
+                          'Choose date',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _submitData,
+                  child: Text('Add Transaction'),
+                )
+              ]),
+        ),
       ),
     );
   }
