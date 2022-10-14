b21c08c985840b9c6f2ebb7af9e6d25bfc215760
Dmytro Holysh <dima.golysh@gmail.com>
2022-10-14 10:24:23 +0200

    responsive ui for landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import './widgets/chart.dart';
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
 import './models/transaction.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  //   DeviceOrientation.portraitDown,
+  // ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -69,6 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
     //   date: DateTime.now(),
     // ),
   ];
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
     return _userTransactions
@@ -114,6 +123,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     final appBar = AppBar(
       title: const Text('Personal Expenses'),
       actions: <Widget>[
@@ -122,10 +133,17 @@ class _MyHomePageState extends State<MyHomePage> {
             icon: const Icon(Icons.add))
       ],
     );
-
     final availableHeight = MediaQuery.of(context).size.height -
         appBar.preferredSize.height -
         MediaQuery.of(context).padding.top;
+
+    final txListWidget = Container(
+      height: availableHeight * 0.7,
+      child: TransactionList(
+        transactions: _userTransactions,
+        removeCallback: _deleteTransaction,
+      ),
+    );
 
     return Scaffold(
       floatingActionButton: FloatingActionButton(
@@ -139,16 +157,34 @@ class _MyHomePageState extends State<MyHomePage> {
             // mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Container(
-                height: availableHeight * 0.35,
-                child: Chart(recentTransactions: _recentTransactions),
-              ),
-              Container(
-                height: availableHeight * 0.65,
-                child: TransactionList(
-                    transactions: _userTransactions,
-                    removeCallback: _deleteTransaction),
-              ),
+              if (isLandscape)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Show Chart'),
+                    Switch(
+                      value: _showChart,
+                      onChanged: (val) {
+                        setState(() {
+                          _showChart = val;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              if (!isLandscape)
+                Container(
+                  height: availableHeight * 0.3,
+                  child: Chart(recentTransactions: _recentTransactions),
+                ),
+              if (!isLandscape) txListWidget,
+              if (isLandscape)
+                _showChart
+                    ? Container(
+                        height: availableHeight * 0.7,
+                        child: Chart(recentTransactions: _recentTransactions),
+                      )
+                    : txListWidget
             ]),
       ),
     );
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,54 +12,57 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: transactions.isEmpty
-            ? Column(
-                children: <Widget>[
-                  Text(
-                    "No transactions added yet",
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    height: 200,
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: constraints.maxHeight * 0.6,
                     child: Image.asset(
-                      "assets/images/waiting.png",
+                      'assets/images/waiting.png',
                       fit: BoxFit.cover,
-                    ),
-                  ),
-                ],
-              )
-            : ListView.builder(
-                itemBuilder: (context, index) => Card(
-                  elevation: 6,
-                  margin: EdgeInsets.all(2),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                            child: Text("\$${transactions[index].amount}")),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => removeCallback(transactions[index].id),
-                    ),
+                    )),
+              ],
+            );
+          })
+        : ListView.builder(
+            itemBuilder: (context, index) => Card(
+              elevation: 6,
+              margin: EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 5,
+              ),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Padding(
+                    padding: const EdgeInsets.all(6),
+                    child: FittedBox(
+                        child: Text("\$${transactions[index].amount}")),
                   ),
                 ),
-                itemCount: transactions.length,
-              ));
+                title: Text(
+                  transactions[index].title,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                subtitle: Text(
+                  DateFormat.yMMMd().format(transactions[index].date),
+                ),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  color: Theme.of(context).errorColor,
+                  onPressed: () => removeCallback(transactions[index].id),
+                ),
+              ),
+            ),
+            itemCount: transactions.length,
+          );
   }
 }
